7545939bc6ee0af580eadc52ee0efd01a553fa6c
Patricia Takehara <paty.takeharab@outlook.com>
2023-09-04 15:15:20 -0300

    Ajuste commit

--- scripts.js
+++ scripts.js
@@ -24,3 +24,42 @@ function cliqueNoBotao() {
 
     buscarCidade(cidade)
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    const inputElement = document.getElementById('dados');
+
+    const awesomplete = new awesomplete(inputElement,{
+        minChars:2, //Numero mínimo de caracteres para começar a exibir sugestões
+        maxItems:10, //Número máximo de itens exibidos na lista de sugestões
+        filter:function(text,input){
+            return awesomplete.FILTER.CONTAINS(text, input.match(/[^,]*$/)[0]);
+        },
+        replace: function(selected){
+            const val = inputElement.value;
+            const before = val.match (/^.+,\s*|/)[0];
+            inputElement.value = before + selected.label + ',';
+        },
+
+    });
+
+    inputElement.addEventListener('input',function(){
+        const inputValue = inputElement.value.trim();
+
+        if (inputValue.length >=2){
+            fetch(`https://api.openweathermap.org/data/2.5/find?q=${inputValue}&type=like&appid=${key}`)
+                .then(response => response.json())
+                .then(data=>{
+                    const citySuggestions = data.list.map(city => ({
+                        label: `${cidade.nome}, ${city.sys.country}`,
+                        value: cidade.nome,
+                    }));
+                    awesomplete.list = citySuggestions;
+                })
+                .catch(error =>{
+                    console.error('Erro ao buscar sugestões de cidades:', error);
+                });
+        }else{
+            awesomplete.list =[];
+        }
+    });
+});
